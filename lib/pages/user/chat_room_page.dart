201427a7f2f8d49dbe9966ccada153f6aa043c08
RakhimyanTim <rahimtoy@gmail.com>
2020-02-18 23:14:16 +0300

    Added messaging option

---
+++ lib/pages/user/chat_room_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:vpomosh/pages/user/chat_screen.dart';
+
+class ChatRoomPage extends StatefulWidget {
+
+  final String peerId;
+  final String currentId;
+  final String peerName;
+
+  ChatRoomPage({
+    this.peerId,
+    this.currentId,
+    this.peerName,
+  });
+
+  @override
+  _ChatRoomPageState createState() => _ChatRoomPageState(
+    peerId: this.peerId,
+    currentId: this.currentId,
+    peerName: this.peerName,
+  );
+}
+
+class _ChatRoomPageState extends State<ChatRoomPage> {
+
+  final String peerId;
+  final String currentId;
+  final String peerName;
+
+  _ChatRoomPageState({
+    this.peerId,
+    this.currentId,
+    this.peerName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text('Чат'),
+        automaticallyImplyLeading: true,
+        centerTitle: false,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 20.0,
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      backgroundColor: Colors.white,
+      body: ChatScreen(
+        peerId: peerId,
+        currentId: currentId,
+        peerName: peerName,
+      ),
+    );
+  }
+}
--- lib/pages/user/user_messages_page.dart
+++ lib/pages/user/user_messages_page.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:vpomosh/pages/user/chat_room_page.dart';
 
 class UserMessagesPage extends StatefulWidget {
 
@@ -14,6 +17,8 @@ class _UserMessagesPageState extends State<UserMessagesPage> {
 
   final FirebaseUser user;
   _UserMessagesPageState({this.user});
+
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +39,119 @@ class _UserMessagesPageState extends State<UserMessagesPage> {
         backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.white,
-      body: null,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            child: new StreamBuilder(
+              stream: Firestore.instance
+                  .collection("users")
+                  .document(user.uid)
+                  .collection("messages")
+                  .snapshots(),
+              builder: (context, snapshot) {
+
+                if (!snapshot.hasData)
+                  return new Container(
+                    alignment: FractionalOffset.center,
+                    child: Center(
+                        child: Theme.of(context).platform == TargetPlatform.iOS
+                            ? new CupertinoActivityIndicator()
+                            : new CircularProgressIndicator()
+                    ),
+                  );
+
+                if (snapshot.data.documents.length == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: Image.asset('assets/images/empty.png'),
+                        ),
+                        ListTile(
+                          title: new Text(
+                            'Нет сообщений',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) => buildItem(
+                      context: context,
+                      document: snapshot.data.documents[index],
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          Positioned(
+            child: isLoading
+                ? Container(
+              child: Center(
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? new CupertinoActivityIndicator()
+                    : new CircularProgressIndicator(),
+              ),
+              color: Colors.white.withOpacity(0.8),
+            )
+                : Container(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildItem({BuildContext context, document}) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: new Container(
+              child: new CircleAvatar(
+                child: document['userName'] != null
+                    ? new Text('${document['userName'][0]}')
+                    : new Text('${document['userName']}'),
+              ),
+            ),
+            title: document['userName'] != null
+                ? new Text(
+              '${document['userName']}',
+              style: TextStyle(color: Colors.black),
+            )
+                : new Text(
+              'Имя',
+              style: TextStyle(color: Colors.black),
+            ) ,
+            trailing: new Icon(Icons.chevron_right, color: Colors.black,),
+
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ChatRoomPage(
+                    peerId: document['userId'],
+                    currentId: widget.user.uid,
+                    peerName: document['userName']!= null
+                        ? document['userName']
+                        : 'Имя',
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
