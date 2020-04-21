1545d9b2daee8e902df409287847b8922778352b
RakhimyanTim <rahimtoy@gmail.com>
2020-04-21 23:28:44 +0300

    Clean code

--- lib/pages/user/user_search_page.dart
+++ lib/pages/user/user_search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -50,7 +51,7 @@ class _UserSearchPageState extends State<UserSearchPage> {
               });
             },
           ),
-          automaticallyImplyLeading: true,
+          automaticallyImplyLeading: false,
           iconTheme: IconThemeData(
             color: Colors.black,
           ),
@@ -96,9 +97,10 @@ class _UserSearchPageState extends State<UserSearchPage> {
                   );
                 return Padding(
                   padding: EdgeInsets.only(top: 10),
-                  child: ListView.builder(
+                  child: GridView.builder(
                     shrinkWrap: true,
                     physics: ClampingScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
                     itemCount: snapshot.data.documents.length,
                     itemBuilder: (context, index) => _buildSearchItem(context, snapshot.data.documents[index], ),
                   ),
@@ -113,6 +115,109 @@ class _UserSearchPageState extends State<UserSearchPage> {
 
   Widget _buildSearchItem(BuildContext context, DocumentSnapshot ad) {
     return Padding(
+      padding: EdgeInsets.all(3.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatRoomPage(
+                peerId: ad['ownerId'],
+                currentId: user.uid,
+                peerName: ad['ownerName'],
+              ),
+            ),
+          );
+        },
+        child: Stack(
+          children: <Widget>[
+            CachedNetworkImage(
+              imageUrl: ad['images'][0],
+              imageBuilder: (context, imageProvider) => Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              placeholder: (context, url) => Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoActivityIndicator()
+                  : Center(child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor)),
+              ),
+            ),
+            Positioned(
+              bottom: 0.0,
+              child:  Container(
+//                decoration: BoxDecoration(
+//                  borderRadius: BorderRadius.all(Radius.circular(10)),
+//                  boxShadow: [
+//                    BoxShadow(
+//                      color: Colors.black.withOpacity(0.1),
+//                    ),
+//                  ],
+//                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '${ad['adServiceName']}',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '${ad['adCategoryName']}',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0.0,
+              top: 0.0,
+              child: Container(
+//                decoration: BoxDecoration(
+//                  borderRadius: BorderRadius.all(Radius.circular(10)),
+//                  boxShadow: [
+//                    BoxShadow(
+//                      color: Colors.black.withOpacity(0.1),
+//                    ),
+//                  ],
+//                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    '${ad['price']} ₽',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    /*return Padding(
       padding: EdgeInsets.all(5),
       child: Column(
         children: <Widget>[
@@ -178,6 +283,6 @@ class _UserSearchPageState extends State<UserSearchPage> {
           Divider(),
         ],
       ),
-    );
+    );*/
   }
 }
